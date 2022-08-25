7414f4e8816ed3c09a96cbd109fdf9b285d00d53
Dumebi Okolo <okolodumebi@gmail.com>
2022-08-25 15:18:11 +0100

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 function Button(props) {
-  return <button>{props.isLoggedIn ? "Login" : "Register"}</button>;
+  
+   const [isHoverEffect, setHoverEffect] = React.useState(false);
+
+
+
+  function setHover() {
+    setHoverEffect(true);
+  }
+  function setHover2() {
+    setHoverEffect(false);
+  }
+
+  return <button style={{backgroundColor: isHoverEffect? "white" : "black"}} onMouseOver={setHover} onMouseOut={setHover2}>{props.isLoggedIn ? "Login" : "Register"}</button>;
 }
 
 export default Button;
